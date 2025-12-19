61a62761c6ba43c35945f17bfe9acc414defc488
mensch777robotics <menschrobotics11@gmail.com>
2025-12-19 13:29:32 +0530

    configured browser to look as www.menschrobotics.com

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const base = mode === 'production' ? '/Mensch_website/' : '/';
+    const base = '/';
     return {
       base,
       server: {
